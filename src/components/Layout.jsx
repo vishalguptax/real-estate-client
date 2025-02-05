7bc75e6e1d0b9ac3f31cedeb6539e3aa417c6e82
Pooja lalwani <you@example.com>
2025-02-05 15:28:22 +0530

    about page created

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,10 +3,11 @@ import React from "react";
 function Layout() {
   return (
     <div className="flex items-center justify-between h-[50px] p-4 bg-black text-white">
-      <div className="left flex gap-4 items-center gap-[50px]">
+      <div className="left flex items-center gap-[50px]">
         <a href="/">Logo</a>
         <a href="/">Home</a>
         <a href="/">About</a>
+
       </div>
       <div className="right flex gap-4 items-center ">
         <a href="/">Login</a>
