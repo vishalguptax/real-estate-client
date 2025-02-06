0aff7af3deb35b4d7de7660d4c44f1ca98345bf8
Anupama <anupamarawat2002@gmail.com>
2025-02-06 15:28:25 +0530

    fixed home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Search from "./Search";
 
-const home = () => {
+function Home() {
   return (
-    <div>home</div>
-  )
+    <div className="flex h-screen">
+      {/* Text Container */}
+      <div className="flex relative flex-1">
+        <img
+          src="./background.svg"
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+
+        <div className="relative z-10 flex-1 flex flex-col justify-center items-center text-center px-6 sm:px-10 md:px-16 lg:px-24">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white ">
+            Find Real Estate & Get Your Dream Place
+          </h1>
+          <p className="mt-4 text-sm sm:text-lg md:text-xl lg:text-2xl text-white font-semibold max-w-2xl">
+            Find the perfect property that fits your lifestyle and budget.
+            Whether you're looking to buy, sell, or rent, we make the process
+            simple and hassle-free.
+          </p>
+        </div>
+        <Search />
+      </div>
+    </div>
+  );
 }
 
-export default home
+export default Home;
