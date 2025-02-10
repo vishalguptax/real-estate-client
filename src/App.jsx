26d2c226297d110253051a404d790e61a09345f2
anvaygupta1940 <guptaanvay1940@gmail.com>
2025-02-10 16:51:28 +0530

    change in importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout.jsx";
-import Home from "./pages/Home/Home.jsx";
-import Profile from "./pages/Profile/Profile.jsx";
-import Register from "./pages/auth/Register.jsx"
-import Login from "./pages/auth/Login.jsx";
-import ProtectedRoutes from "./pages/auth/ProtectedRoutes.jsx";
-import ListItem from "./components/listIems/ListItem.jsx";
-import SinglePage from "./pages/singlepage/singlepage.jsx";
+import Layout from "./components/Layout";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Register from "./pages/auth/Register"
+import Login from "./pages/auth/Login";
+import ProtectedRoutes from "./pages/auth/ProtectedRoutes";
+import ListItem from "./components/listIems/ListItem";
+import SinglePage from "./pages/singlepage/singlepage";
 
 
 function App() {
