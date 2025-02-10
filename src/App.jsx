b992f712ea4b8e0c7dd6ce67f370a328b1f4af04
Pooja lalwani <you@example.com>
2025-02-10 11:21:10 +0530

    trying to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home/Home";
-import About from "./pages/about/about";
-import Contact from "./pages/Contact/contact";
 import Profile from "./pages/Profile/Profile";
 import Register from "./pages/auth/Register"
 import Login from "./pages/auth/Login";
-import SinglePage from './pages/singlepage/singlepage'
+import SinglePage from "./pages/singlepage/singlepage";
+
 
 
  function App() {
@@ -16,14 +15,12 @@ import SinglePage from './pages/singlepage/singlepage'
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/singlepage" element={<SinglePage/>}/>
-            </Route>
-         </Routes>
+          </Route>
+        </Routes>
       </>
     </Router>
    )
