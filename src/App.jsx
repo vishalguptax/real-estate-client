9b094cd7ba4e917b8fe3a8e9599cb307fd8ce588
anvaygupta1940 <guptaanvay1940@gmail.com>
2025-02-10 16:30:19 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Profile from "./pages/Profile/Profile.jsx";
 import Register from "./pages/auth/Register.jsx"
 import Login from "./pages/auth/Login.jsx";
 import ProtectedRoutes from "./pages/auth/ProtectedRoutes.jsx";
+import ListItem from "./components/listIems/ListItem.jsx";
 
 
 function App() {
   return (
-    
-        <Routes>
-          <Route element={<Layout />}>
-            <Route element ={<ProtectedRoutes/>}>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/profile" element={<Profile/>}/>
-            </Route>
-            
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/login" element={<Login/>}/>
-          </Route>
-        </Routes>
-     
+
+    <Routes>
+      <Route element={<Layout />}>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/listItem" element={<ListItem></ListItem>}></Route>
+      </Route>
+    </Routes>
+
   );
 }
 
