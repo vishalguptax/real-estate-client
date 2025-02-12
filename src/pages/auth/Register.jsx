3d6974f0f12f90ca5a5e310825d4f04775b63d32
ei-csa0 <eio52588@gmail.com>
2025-02-12 13:08:57 +0530

    updated form

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useFormik } from "formik";
 import RegisterSchema from "./schema/registerSchema";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
 
 const initialValues = {
@@ -45,22 +45,17 @@ const Register = () => {
     });
 
   return (
-
-    <div className="image-bg  flex items-center justify-center">
-
-      <div className="max-h-fit  flex items-center  overflow-auto custom-scrollbar w-[80%] ml-auto mr-auto max-w-md  bg-white/20 shadow-xl rounded-lg ring-2 ring-black/20 ">
-        <form
-          className="w-full max-h-fit dark:bg-gray-700/20 p-4"
-          onSubmit={handleSubmit}
-          autoComplete="off"
-        >
+    <div className="form-bg">
+      <div className="max-h-[90vh] my-10 overflow-auto custom-scrollbar flex justify-center w-[80%] ml-auto mr-auto max-w-md bg-white/20 shadow-xl rounded-lg ring-2 ring-black/20">
+        <form className="w-full p-5" onSubmit={handleSubmit} autoComplete="off">
           <h1 className="text-center text-2xl font-bold text-black m-4">
             Sign Up
           </h1>
-          <div className=" w-full max-h-fit">
+          <div className="items-center m-3">
+       
             <div className="register-div-input">
               <input
-                className={`login-input mb-2 ${
+                className={`form-input ${
                   errors.name && touched.name
                     ? "border-red-500"
                     : "border-gray-500"
@@ -72,15 +67,15 @@ const Register = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-
               {errors.name && touched.name && (
                 <p className="error-message">{errors.name}</p>
               )}
             </div>
 
+           
             <div className="register-div-input">
               <input
-                className={`login-input mb-2 ${
+                className={`form-input ${
                   errors.email && touched.email
                     ? "border-red-500"
                     : "border-gray-500"
@@ -97,9 +92,10 @@ const Register = () => {
               )}
             </div>
 
+         
             <div className="register-div-input">
               <input
-                className={`login-input mb-2 ${
+                className={`form-input ${
                   errors.mobileNumber && touched.mobileNumber
                     ? "border-red-500"
                     : "border-gray-500"
@@ -117,9 +113,9 @@ const Register = () => {
             </div>
 
             <div className="sm:flex-row md:gap-8 md:flex">
-              <div className="relative register-div-input w-1/2">
+              <div className="relative register-div-input">
                 <input
-                  className={`login-input mb-2 ${
+                  className={`form-input ${
                     errors.password && touched.password
                       ? "border-red-500"
                       : "border-gray-500"
@@ -132,7 +128,7 @@ const Register = () => {
                   onBlur={handleBlur}
                 />
                 <i
-                  className="absolute right-3 top-9 transform -translate-y-1/2 cursor-pointer"
+                  className="eye-icon"
                   onClick={togglePasswordVisibility}
                 >
                   <FontAwesomeIcon
@@ -144,9 +140,9 @@ const Register = () => {
                 )}
               </div>
 
-              <div className="relative register-div-input w-1/2">
+              <div className="relative register-div-input">
                 <input
-                  className={`login-input mb-2 ${
+                  className={`form-input ${
                     errors.confirmPassword && touched.confirmPassword
                       ? "border-red-500"
                       : "border-gray-500"
@@ -159,7 +155,7 @@ const Register = () => {
                   onBlur={handleBlur}
                 />
                 <i
-                  className="absolute right-3 top-9 transform -translate-y-1/2 cursor-pointer"
+                  className="eye-icon"
                   onClick={toggleConfirmPasswordVisibility}
                 >
                   <FontAwesomeIcon
@@ -172,14 +168,15 @@ const Register = () => {
               </div>
             </div>
 
-            <button className="login-button mx-auto block m-4" type="submit">
+            <button className="form-button" type="submit">
               Sign Up!
             </button>
 
-            <p className="text-black text-center m-3">
+        
+            <p className="text-black text-center mt-3">
               Already have an account?
-              <Link to="/Login" className="hover:text-amber-500 underline">
-                {" "}
+              <Link to="/Login" className="hover:text-amber-500 font-bold">
+                 {" "}
                 Sign In{"  "}
               </Link>
             </p>
